585a434fd544b58e5c1c424fc975dd110bc19697
muhammadmasud1 <mdmasudrana15020@gmail.com>
2023-07-28 20:41:17 +0600

    ecommercewebsite

---
+++ css/app.js
@@ -0,0 +1,7 @@
+let nav = document.getElementById('navbar');
+let shownav = document.getElementById('showName');
+
+
+shownav.addEventListener('click', function(){
+            nav.classList.toggle('toggle-nav');
+})
